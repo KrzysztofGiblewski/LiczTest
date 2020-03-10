7472b95cab3f68546bbe9f20a68ff8ad510e6932
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2020-03-10 22:01:04 +0100

    dodana klasa licz i metoda sumuj

---
+++ src/main/java/Licz.java
@@ -0,0 +1,12 @@
+public class Licz {
+    int pierwsza, druga, wynik;
+
+    public int sumuj(int pierwsza, int druga) {
+        this.pierwsza = pierwsza;
+        this.druga = druga;
+        this.wynik = wynik;
+        wynik = pierwsza + druga;
+        return wynik;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int liczba1, liczba2, wynikObu = 0;
+        Licz licz = new Licz();
+        System.out.println("podaj liczbę");
+        Scanner wczytywanie = new Scanner(System.in);
+        liczba1 = wczytywanie.nextInt();
+        System.out.println("podałeś " + liczba1);
+        System.out.println("podaj drugą liczbę");
+        liczba2 = wczytywanie.nextInt();
+        System.out.println("podałeś " + liczba2);
+        wynikObu = licz.sumuj(liczba1, liczba2);
+        System.out.println("suma liczb " + liczba1 + " i liczby " + liczba2 + " to " + wynikObu);
+
+    }
+}
